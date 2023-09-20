c35e4f0010bcaf2bb24859dae74c0f09a6140ff1
Zachary Vandeventer <zachv94@gmail.com>
2023-09-19 21:39:33 -0400

    built basic snake game

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import React, { useState, useEffect } from "react";
 import Snake from ".";
 
 function App() {
-  const [x, setX] = useState(610);
-  const [y, setY] = useState(341.5);
+  const [x, setX] = useState(16);
+  const [y, setY] = useState(16);
   const [direction, setDirection] = useState("left");
-  const [score, setScore] = useState(0);
+  const [apple, setApple] = useState({
+    x: Math.floor(Math.random() * 16) * 2,
+    y: Math.floor(Math.random() * 16) * 2,
+  });
+  const [score, setScore] = useState(2);
+  const [highScore, setHighScore] = useState(0);
   const [start, setStart] = useState(false);
-  const [collided, setCollided] = useState(false);
   const [snakeCoords, setSnakeCoords] = useState([]);
 
   window.addEventListener("keydown", (e) => handleKeyDown(e));
+
+  function randomX() {
+    let rand = Math.floor(Math.random() * 16) * 2;
+    while (snakeCoords.some((n) => n.x === rand))
+      rand = Math.floor(Math.random() * 16) * 2;
+    return rand;
+  }
+
+  function randomY() {
+    let rand = Math.floor(Math.random() * 16) * 2;
+    while (snakeCoords.some((n) => n.y === rand))
+      rand = Math.floor(Math.random() * 16) * 2;
+    return rand;
+  }
 
   function handleKeyDown(e) {
     switch (e.key) {
@@ -29,11 +47,17 @@ function App() {
         break;
       case "Enter":
         if (!start) {
-          setX(610);
-          setY(341.5);
+          setX(16);
+          setY(16);
           setDirection("left");
           setStart(true);
+          setHighScore(score);
+          setScore(0);
           setSnakeCoords([]);
+          setApple({
+            x: randomX(),
+            y: randomY(),
+          });
         }
         break;
       default:
@@ -42,6 +66,7 @@ function App() {
   }
 
   useEffect(() => {
+    console.log(snakeCoords.length);
     if (start) {
       const interval = setInterval(() => {
         setHeadCoordinates();
@@ -50,49 +75,89 @@ function App() {
     }
 
     function setHeadCoordinates() {
-      if (start) {
-        switch (direction) {
-          case "left":
-            if (x - 20 >= 350) {
-              console.log(`(${x}, ${y})`);
-              setSnakeCoords([{ x, y }, ...snakeCoords]);
-              setX(x - 20);
-            } else {
-              setStart(false);
-              setCollided(true);
+      let collision = false;
+      switch (direction) {
+        case "left":
+          if (
+            x - 2 >= 0 &&
+            !snakeCoords.some((n) => n.x === x - 2 && n.y === y)
+          ) {
+            setX(x - 2);
+            if (x - 2 === apple.x && y === apple.y) {
+              setScore(score + 1);
+              setApple({
+                x: randomX(),
+                y: randomY(),
+              });
             }
-            break;
-          case "right":
-            if (x + 20 <= 870) {
-              setSnakeCoords([{ x, y }, ...snakeCoords]);
-
-              setX(x + 20);
-            } else {
-              setStart(false);
-              setCollided(true);
+          } else {
+            setStart(false);
+            collision = true;
+          }
+          break;
+        case "right":
+          if (
+            x + 2 < 34 &&
+            !snakeCoords.some((n) => n.x === x + 2 && n.y === y)
+          ) {
+            setX(x + 2);
+            if (x + 2 === apple.x && y === apple.y) {
+              setScore(score + 1);
+              setApple({
+                x: randomX(),
+                y: randomY(),
+              });
             }
-            break;
-          case "up":
-            if (y - 20 >= 80) {
-              setSnakeCoords([{ x, y }, ...snakeCoords]);
-              setY(y - 20);
-            } else {
-              setStart(false);
-              setCollided(true);
+          } else {
+            setStart(false);
+            collision = true;
+          }
+          break;
+        case "up":
+          if (
+            y - 2 >= 0 &&
+            !snakeCoords.some((n) => n.x === x && n.y === y - 2)
+          ) {
+            setY(y - 2);
+            if (y - 2 === apple.y && x === apple.x) {
+              setScore(score + 1);
+              setApple({
+                x: randomX(),
+                y: randomY(),
+              });
             }
-            break;
-          case "down":
-            if (y + 20 <= 620) {
-              setSnakeCoords([{ x, y }, ...snakeCoords]);
-              setY(y + 20);
-            } else {
-              setStart(false);
-              setCollided(true);
+          } else {
+            setStart(false);
+            collision = true;
+          }
+          break;
+        case "down":
+          if (
+            y + 2 < 34 &&
+            !snakeCoords.some((n) => n.x === x && n.y === y + 2)
+          ) {
+            setY(y + 2);
+            if (y + 2 === apple.y && x === apple.x) {
+              setScore(score + 1);
+              setApple({
+                x: randomX(),
+                y: randomY(),
+              });
             }
-            break;
-          default:
-            return;
+          } else {
+            setStart(false);
+            collision = true;
+          }
+          break;
+        default:
+          return;
+      }
+      if (!collision) {
+        let currentSC = snakeCoords;
+        if (snakeCoords.length > score) {
+          currentSC = snakeCoords.slice(0, score);
         }
+        setSnakeCoords([{ x, y }, ...currentSC]);
       }
     }
   }, [start, x, y]);
@@ -104,24 +169,36 @@ function App() {
         className="snake"
         key="0"
         id="head"
-        style={{ top: y, left: x }}
+        style={{ top: y - 2 + "em", left: x + "em" }}
       ></div>
     );
-    for (let n in snakeCoords) {
+    for (let n = 1; n <= snakeCoords.length; n++) {
       snake.push(
         <div
           className="snake"
-          id={`body ${n + 1}`}
-          key={n + 1}
-          style={{ top: snakeCoords[n].y, left: snakeCoords[n].x }}
+          id={`body ${n}`}
+          key={n}
+          style={{
+            top: snakeCoords[n - 1].y - 2 - 2 * n + "em",
+            left: snakeCoords[n - 1].x + "em",
+          }}
         ></div>
       );
     }
     return snake;
   }
 
+  function placeApple() {
+    return (
+      <div
+        className="apple"
+        style={{ top: apple.y + "em", left: apple.x + "em" }}
+      ></div>
+    );
+  }
+
   function gameOver() {
-    if (!start && collided) {
+    if (!start) {
       return <h1 id="GameOver">Game Over</h1>;
     } else return;
   }
@@ -129,8 +206,15 @@ function App() {
   return (
     <div className="App">
       <div id="gameScoreContainer">
-        <h1 id="score">Score: {score}</h1>
-        <div id="gameScreen">{buildSnake()}</div>
+        <div>
+          <h1 id="score">Score: {score}</h1>
+          <br />
+          <h1 id="highScore">High Score: {highScore}</h1>
+        </div>
+        <div id="gameScreen">
+          {placeApple()}
+          {buildSnake()}
+        </div>
       </div>
       {gameOver()}
     </div>
